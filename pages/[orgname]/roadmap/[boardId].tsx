916528d6e0686f81e795b438819b5952f00a881e
nareshbhusal <devnbhusal@gmail.com>
2022-06-17 11:05:32 +0530

    Add ability to rename boards

--- pages/[orgname]/roadmap/[boardId].tsx
+++ pages/[orgname]/roadmap/[boardId].tsx
@@ -1,11 +1,12 @@
 import { NextPageWithLayout } from '../../../types/page';
 import Layout from '../../../layouts/layout';
 import Head from 'next/head';
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import { v4 as uuid } from 'uuid';
 import {
   Flex,
+  Input,
   Stack,
   Text,
   Heading
@@ -16,13 +17,61 @@ import StoryModal from '../../../components/StoryModal/';
 import { listIdToString } from '../../../components/Column/';
 import { db } from '../../../db';
 import { useLiveQuery } from 'dexie-react-hooks';
-import { TAG_COLORS } from '../../../lib/constants';
+
+const BoardHeading: React.FC<{ name: string; boardId: number; }> = ({ name, boardId }) => {
+  const [isRenaming, setIsRenaming] = useState<boolean>(false);
+  const [boardName, setBoardName] = useState(name);
+  const nameInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isRenaming) {
+      nameInputRef.current!.focus();
+    }
+  }, [isRenaming]);
+
+  return (
+    <>
+      {isRenaming ?
+        <Input
+          width={'300px'}
+          ref={nameInputRef}
+          value={boardName}
+          onChange={(e: any) => setBoardName(e.target.value)}
+          fontWeight={'bold'}
+          fontSize={'lg'}
+          _focus={{
+            border: '1px solid #343',
+          }}
+          onBlur={async () => {
+            if (boardName.trim()) {
+              await db.updateBoard(boardId, {
+                name: boardName.trim()
+              });
+            }
+            setIsRenaming(false);
+          }}
+          bg={'#fff'}
+          color={'#111'}
+        /> :
+          <Heading
+            p={'4px'}
+            onClick={() => setIsRenaming(true)}
+            as={'h2'}
+            color={'#fff'}
+            size={'md'}
+          >
+            {boardName}
+          </Heading>
+      }
+    </>
+  );
+}
 
 const Kanban: NextPageWithLayout = () => {
   const router = useRouter();
   const boardId = parseInt(router.query.boardId as string);
 
-  let boardData = null;
+  let boardData: any = null;
   // const [boardData, setBoardData] = useState<any>(null);
   const [randomString, setRandomString] = useState(uuid());
   // window.alert(uuid())
@@ -63,14 +112,7 @@ const Kanban: NextPageWithLayout = () => {
         <Stack
           pl={'30px'}
           flexGrow={1}>
-          <Heading
-            p={'4px'}
-            as={'h2'}
-            color={'#fff'}
-            size={'md'}
-          >
-            {boardData.name}
-          </Heading>
+          <BoardHeading boardId={Number(boardId)} name={boardData.name} />
           <Board refreshData={refreshData} boardData={boardData} />
         </Stack>:
         <Text>Loading...</Text>
